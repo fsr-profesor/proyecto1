4e78fff50707b766da4591c872c9f52b5172b4f3
Fran <franciscosanchez@iesantoniogala.es>
2022-03-17 10:08:23 +0100

    Primer commit en testing

--- src/fsr/HolaMundo.java
+++ src/fsr/HolaMundo.java
@@ -5,6 +5,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		
+		System.out.println("Esta es la rama Testing");
+		
 
 	}
 
